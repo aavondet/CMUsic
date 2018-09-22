3c1091b0f4b83b12d230fc8081a4c285f694cc78
Arnaud <arnaud@Arnauds-Air.wv.cc.cmu.edu>
2018-09-22 15:35:01 -0400

    almost there

--- hackcmu.py
+++ hackcmu.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import request
 # Create the application.
 APP = flask.Flask(__name__)
 
@@ -8,11 +8,24 @@ def index():
     """ Displays the index page accessible at '/'
     """
     return flask.render_template('index_start.html')
-    
-@APP.route('/input')
+
+users = []
+@APP.route('/submit', methods=['POST'])
 def index2():
+    if request.method == 'POST':
+        new_user={}
+        json = request.get_json()
+        new_user["andrewId"] = json["id"]
+        new_user["artists"] = json["artist_list"]
+        new_user["genres"] = json["genre_list"]
+        new_user["songs"] = json["song_list"]
+        print(new_user)
     """ Displays the index page accessible at '/'
     """
+    return flask.render_template('index_input.html')
+
+@APP.route('/input')
+def index4():
     return flask.render_template('index_input.html')
 
 @APP.route('/result')
--- static/main_start.js
+++ static/main_start.js
@@ -4,15 +4,15 @@ var basicTimeline = anime.timeline({
 );
 basicTimeline
 .add({
-    targets: '.title',
+    targets: '#logo',
     easing: 'easeInOutSine',
     scale: [300, 1],
     opacity: [0,1],
-    duration: 3000
+    duration: 2000
   })
 .add({
     targets: '.buttons',
     easing: 'easeOutExpo',
     opacity: [0,1],
-    duration: 5000
+    duration: 4000
 })
